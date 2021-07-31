5ccfe5703e4054f6c6fe9a3042e624ecf54f94bb
sujithra <sujithra200019@gmail.com>
2021-08-01 00:04:29 +0530

    i

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 var userid='';
 var courseId ='';
 
+var searchUsers = document.querySelector('#search-users');
+    
+    
+searchUsers.addEventListener('keydown', myfun);
+
+function myfun(event)
+{
+    users = document.querySelectorAll('.cardTemp'),
+    usersData = document.querySelectorAll('.searchtitle'),
+    
+//alert(users.length);
+    searchVal =event.target.value.toLowerCase();
+    console.log(searchVal);
+    console.log(usersData[0].textContent);
+    //alert(usersData[0].textContent);
+    for (var i = 0; i < users.length; i++) {
+        if (!searchVal || usersData[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
+            users[i].style['display'] = 'flex';
+          }
+          else {
+            users[i].style['display'] = 'none';
+          }
+    }
+}
+
+
 function login(){
 
     let username='';
@@ -38,14 +64,16 @@ function login(){
                 }
             });
 }
-document.body.addEventListener("click",()=>buy());
+//document.body.addEventListener("click",()=>buy());
 
 //alert(document.cookie);
 
 function buy(){
+    var enrolled_users;
     let enrollButton=event.target;
     if(enrollButton.className.includes('buy')){
-        let courseid=enrollButton.getAttribute("data-id");
+        let courseid=sessionStorage.getItem("courseId");
+        let id=sessionStorage.getItem("loggedUserId");
         let progress=0;
         let data_header={};
         var data= {
@@ -66,13 +94,97 @@ function buy(){
                 if(!response.ok){
                     throw Error(response.status)
                 }
+            //    alert("enrolled successfully");
                 return response.json();
             })
             .then((data)=>{
                 console.log(data);
-            })
+            });
+
+            addUser();
+
+            
+//         fetch("http://localhost:3000/courses").then(res=>res.json()).then(
+//                 courseResponse=>{
+//                   //  res=courseResponse;
+//                     alert("hiiiiiiiiiiiiii");
+//                     courseResponse.forEach(course=>{
+//                         alert(typeof(courseId));
+//                         alert(course.id);
+
+//                         if(courseId==course.id){
+//                             enrolled_users=course.enrolled_users;
+//                             alert(enrolled_users);
+// //                            console.log(enrolled_users);
+//                         }
+//                     })
+//                 }
+//         ) 
+
+       // console.log(enrolled_users);   
     }
+    location.href="course_page.html";
 }
+
+function addUser(){
+    alert("hiii");
+    let enrolled_users = [];
+    let courseid=sessionStorage.getItem("courseId");
+    let url='http://localhost:3000/courses/'+courseid;
+    alert(url);
+    fetch(url).then(
+        response =>{
+            if(response.ok){
+                return response.json();
+            }}).then(userResponse=>{
+                alert("user hi ");
+                userDetails = userResponse;
+                alert(userDetails.enrolled_users);
+            //    enrolled_users = userDetails.enrolled_users;
+            //     addusertolist(enrolled_users,courseid);
+
+                // userResponse.forEach(course=>{
+                //     alert("hihihihih");
+                //                             alert(typeof(courseId));
+                //                             alert(course.id);
+                    
+                //                             if(courseId==course.id){
+                //                                 enrolled_users=course.enrolled_users;
+                //                                 alert(enrolled_users);
+                //     //                            console.log(enrolled_users);
+                //                             }
+                //                         }
+
+                //                     )
+                                });
+                              //  alert(enrolled_users);
+}
+
+
+
+function addusertolist(enrolled_users,id)
+{
+    alert("inside fun");
+    enrolled_users.append(sessionStorage.getItem("loggedUserId"));
+   // let id = event.target.parentNode.getAttribute('id');
+    
+          ///////put
+         // let name = 'http://localhost:3000/employees/'+id;
+          //console.log(name);
+          fetch('http://localhost:3000/courses/'+id, {
+            method: 'PATCH',
+            body: JSON.stringify({
+           enrolled_users:enrolled_users
+        }),
+        headers: {
+    'Content-type': 'application/json; charset=UTF-8'
+}
+})
+.then(response => response.json())
+.then(json => console.log(json))
+}
+
+
 
 
 function setup(){
@@ -80,6 +192,7 @@ function setup(){
     let start_buyButton=document.querySelector("#status");
     var cid=sessionStorage.getItem("courseId");
     start_buyButton.innerHTML="Buy Now";
+    start_buyButton.className="btn btn-primary btn-sm btn-block buy";
     userid=sessionStorage.getItem('loggedUserId')
                 
      fetch("http://localhost:3000/user_course")
@@ -89,7 +202,7 @@ function setup(){
             if(uc.userid==userid&&uc.courseid==cid){
                 console.log(userid);
                 start_buyButton.innerHTML="Start Learning";
-            
+                start_buyButton.className="btn btn-primary btn-sm btn-block";
         }
          });
         })
@@ -118,6 +231,8 @@ function setup(){
 
 
 function loadMainCourse(){
+    var allCourses = document.getElementById('names-all');
+    
   // console.log(sessionStorage.getItem('loggedUserId'));
     let count = 0;
     fetch(" http://localhost:3000/courses")
@@ -131,7 +246,7 @@ function loadMainCourse(){
        return courses.map((course)=>{
         //console.log( course.name);
           if(course.rating>=4.5&&(count++)<3){  
-            showCourse(course.id,course.name,course.desc,course.category,course.tutor,course.price,course.rating);
+            showCourse(course.id,course.name,course.desc,course.category,course.tutor,course.price,course.rating,allCourses);
           }
            });
       }).catch(()=>{console.log("Error in response");});
@@ -141,6 +256,10 @@ function loadMainCourse(){
 
 
 function loadCourse(){
+    userid= sessionStorage.getItem("loggedUserId");
+    var allCourses = document.getElementById('allCourses');
+    var myCourses = document.getElementById('myCourses');
+
 fetch(" http://localhost:3000/courses")
 .then((response)=>response.json())
  .then((res_json)=>{
@@ -151,8 +270,14 @@ fetch(" http://localhost:3000/courses")
       // }
        return courses.map((course)=>{
         //console.log( course.name);
-          showCourse(course.id,course.name,course.desc,course.category,course.tutor,course.price,course.rating);
-           });
+        var enrolled_users=course.enrolled_users;
+            if(enrolled_users.includes(userid)){
+                showCourse(course.id,course.name,course.desc,course.category,course.tutor,course.price,course.rating,myCourses);
+            }
+            else{
+                showCourse(course.id,course.name,course.desc,course.category,course.tutor,course.price,course.rating,allCourses);
+            }
+       })
 
  }).catch(()=>{console.log("Error in response");});
 }
@@ -160,24 +285,24 @@ fetch(" http://localhost:3000/courses")
 
 
 
-function showCourse(id,s1,s2,s3,s4,p1,r1)
+function showCourse(id,s1,s2,s3,s4,p1,r1,nameItems)
 {
 
 var cartRow2 = document.createElement('div');
 cartRow2.classList="col-12 col-sm-12 col-md-4 ";
 
            
-var nameItems = document.getElementById('names-all');
+//var nameItems = document.getElementById('names-all');
 
  
   var allnames = `
-                    <div class="card border-dark mb-3" style="width: 20rem;">\
+                    <div class="card cardTemp border-dark mb-3" style="width: 20rem; display:flex">\
                     <center>\
                         <div class="card-img-top feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>\
                         <!-- <img class="card-img-top" src="apple.jfif" alt="Card image cap"> -->\
                         <div class="card-block">\
                         <center>\
-                          <h4 class="card-title card-header h4 fw-bolder my_text">${s1}</h4>\
+                          <h4 class="card-title card-header h4 fw-bolder my_text searchtitle">${s1}</h4>\
                           <span class="card-subtitle mb-2 text-muted">${s3}</span>\
                           <blockquote class="blockquote mb-0">\
                           <p class="card-text my_text ">${s2}</p>\
@@ -205,6 +330,8 @@ cartRow2.innerHTML = allnames;
 nameItems.appendChild(cartRow2);
 cartRow2.getElementsByClassName('enrollNow')[0].addEventListener('click', enrollCourse)
 console.log(id);
+
+
 
 }
 
